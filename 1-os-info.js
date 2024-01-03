a2252a3caa8894af62d44d1f61b16ffc5f07fa40
Fercho <ferchinandres@gmail.com>
2024-01-03 09:19:36 -0500

    this

---
+++ 1-os-info.js
@@ -0,0 +1,12 @@
+const os = require('node:os') 
+
+console.log('Inf del sistema operativo')
+console.log('---------------')
+
+console.log('Nombre del sistema operativo', os.platform())
+console.log('Ver. del sistema operativo', os.release())
+console.log('Arquitectura', os.arch())
+console.log('CPUs', os.cpus())
+console.log('Memoria Libre', os.freemem() / 1024 / 1024)
+console.log('Memoria total' , os.totalmem / 1024 / 1024)
+console.log('Uptime', os.uptime() / 60 / 60 ) 
---
+++ 1-os-info.mjs
@@ -0,0 +1,12 @@
+import { platform, release, arch, cpus, freemem, totalmem, uptime } from 'node:os' 
+
+console.log('Inf del sistema operativo')
+console.log('---------------')
+
+console.log('Nombre del sistema operativo', platform())
+console.log('Ver. del sistema operativo', release())
+console.log('Arquitectura', arch())
+console.log('CPUs', cpus())
+console.log('Memoria Libre', freemem() / 1024 / 1024)
+console.log('Memoria total' , totalmem / 1024 / 1024)
+console.log('Uptime', uptime() / 60 / 60 ) 
